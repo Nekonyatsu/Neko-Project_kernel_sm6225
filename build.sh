9e3451783988e07aed05ed16ecb02e7602841aaf
anomaly-4rc <miquu.official@gmail.com>
2026-06-04 01:29:28 +0700

    refactor: Update build script for improved kernel compilation process

--- build.sh
+++ build.sh
@@ -1,78 +1,51 @@
 #!/bin/bash
-#
-# Compile script for vauxite Kernel
-# Copyright (C) 2020-2021 Adithya R.
-
-SECONDS=0 # builtin bash timer
-ZIPNAME="vauxite-$(date '+%Y%m%d-%H%M')-spes.zip"
-TC_DIR="$(pwd)/tc/clang-r450784e"
-AK3_DIR="$(pwd)/android/AnyKernel3"
-DEFCONFIG="vendor/spes-perf_defconfig"
-
-if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-   head=$(git rev-parse --verify HEAD 2>/dev/null); then
-	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
-fi
-
+# script BY: anomaly_arc
+SECONDS=0 
+DEFCONFIG="vendor/fog-perf_defconfig"
+export KBUILD_BUILD_USER="Filia-Lunae"
+export KBUILD_BUILD_HOST="Linux_Mint_22.3-Cinnamon_64-bit"
+export ARCH=arm64
+export SUBARCH=arm64
+TC_DIR="$(pwd)/../android_prebuilts_clang-standalone"
 export PATH="$TC_DIR/bin:$PATH"
-export KBUILD_BUILD_USER=nobody
-export KBUILD_BUILD_HOST=android-build
-
-if ! [ -d "$TC_DIR" ]; then
-	echo "AOSP clang not found! Cloning to $TC_DIR..."
-	if ! git clone --depth=1 -b 14 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
-		echo "Cloning failed! Aborting..."
-		exit 1
-	fi
-fi
-
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-	cp out/defconfig arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
-	exit
-fi
-
-if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG
-	cp out/.config arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated full defconfig at $DEFCONFIG"
-	exit
-fi
+export USE_CCACHE=1
+export CCACHE_DIR="/media/filia/DiskB/.ccache"
+export CC="ccache clang"
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
-	rm -rf out
+        echo "Cleaning up out folder..."
+        rm -rf out
 fi
 
 mkdir -p out
+echo -e "\nGenerating defconfig: $DEFCONFIG"
 make O=out ARCH=arm64 $DEFCONFIG
 
-echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 Image.gz dtb.img dtbo.img 2> >(tee log.txt >&2) || exit $?
+echo -e "\nStarting compilation on 4 Cores (-j4) via LLVM..."
+make -j4 O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 Image.gz dtbs
 
 kernel="out/arch/arm64/boot/Image.gz"
-dtb="out/arch/arm64/boot/dtb.img"
-dtbo="out/arch/arm64/boot/dtbo.img"
+dtb_dir="out/arch/arm64/boot/dts/vendor/qcom"
+output_final="out/Image.gz-dtb"
+dtb="out"
 
 if [ -f "$kernel" ]; then
-	echo -e "\nKernel compiled succesfully! Zipping up...\n"
-	if [ -d "$AK3_DIR" ]; then
-		cp -r $AK3_DIR AnyKernel3
-	elif ! git clone -q https://github.com/CHRISL7/AnyKernel3 -b master; then
-		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
-		exit 1
-	fi
-	cp $kernel $dtb $dtbo AnyKernel3
-	rm -rf out/arch/arm64/boot
-	cd AnyKernel3
-	git checkout master &> /dev/null
-	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
-	cd ..
-	rm -rf AnyKernel3
-	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-	echo "Zip: $ZIPNAME"
+        echo -e "\n====================================="
+        echo -e "COMPILE SUCCESSFUL"
+        echo -e "Compilation Time: $((SECONDS / 60)) minute $((SECONDS % 60)) second"
+        echo -e "-------------------------------------"
+        
+        cat "$dtb_dir"/*.dtb > "$dtb/dtb_combined"     
+        if [ -d "$dtb_dir" ] && [ "$(ls -A $dtb_dir/*.dtb 2>/dev/null)" ]; then
+                cat "$kernel" $dtb/dtb_combined > "$output_final"
+                echo -e "EXTRACT & MERGE PROCESS SUCCESSFUL!"
+                echo -e "Flash Ready Final Result: $output_final"
+        else
+                echo -e "Warning: .dtb file not found in $dtb_dir"
+                echo -e "Failed to create Image.gz-dtb, please check your dts configuration."
+        fi
+        echo -e "====================================="
 else
-	echo -e "\nCompilation failed!"
-	exit 1
+        echo "Compilation Failed!"
+        exit 1
 fi
-
